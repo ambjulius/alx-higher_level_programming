94cfb04bba086a4e9d822be9dfaaa5687afdf5a3
ambjulius <itaman.julius@gmail.com>
2022-08-15 16:30:47 +0100

    sql

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/python3
-for letter_c in range(97, 123):
-    print("{}".format(chr(letter_c)), end="")
--- 0x02-python-import_modules/4-hidden_discovery.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python3
-
-if __name__ == '__main__':
-    import hidden_4
-
-    name_list = dir(hidden_4)
-    for st in name_list:
-        if not st.startswith('__'):
-            print('{:s}'.format(st))
